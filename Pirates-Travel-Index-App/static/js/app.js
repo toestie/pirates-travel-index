a7bc342d10ab09965b3e9cb3a53b693dee03f78b
c-l-nguyen <c.l.nguyen@icloud.com>
2019-11-30 15:44:10 -0600

    better chart formatting and split chart creation into function

--- Pirates-Travel-Index-App/static/js/app.js
+++ Pirates-Travel-Index-App/static/js/app.js
@@ -1,23 +1,17 @@
-(async function init() {
+async function plotBump(url, html_id, title){
+  let data = await d3.json(url);
 
-  const tourist_url = "/gdci-tourists";
-  const expenses_url = "/gdci-expenses";
-  const ratio_url = "/tourism-ratio";
-
-  let data = await d3.json(tourist_url);
-  console.table(data);
-  
+  // build the chart
   let chart = new d3plus.BumpChart()
     .data(data)
-    .select("#viz1")
+    .select(html_id)
     .groupBy("City")
-    // .label(d => d.City)
     .label(function(d) {
       if (d.id == "circle"){
         return String(d.Rank)
       }
       else {
-          return d.City;
+        return d.City;
       }
     })
     .x("Year")
@@ -29,28 +23,110 @@
       else {
         return "Circle";
       }
-    });
+  });
 
+  // format the chart
   chart
     .xConfig({
-      title: "Years"
+      title: "Years",
+      tickSize: 15
     })
     .yConfig({
       title: "Ranking",
+      tickSize: 35,
       ticks: []
     })
     .y2Config({
+      tickSize: 35,
       ticks: []
     })
     .shapeConfig({
       Line: {
         strokeWidth: 5
+      },
+      Circle: {
+        r: 17,
+      },
+      labelConfig: {
+        fontMin: 11
       }
     })
-    .sizeMin(20)
-    .title("Top Number of Tourists Ranking")
+    .title(title)
+    .titleConfig({
+      fontSize: 20
+    })
+    .legendPosition("top")
     .render()
   ;
+
+}
+
+(function init() {
+
+  const tourist_url   = "/gdci-tourists";
+  const expenses_url  = "/gdci-expenses";
+  const ratio_url     = "/tourism-ratio";
+
+  plotBump(tourist_url, "#viz1", "Top Number of Tourists Ranking");
+  plotBump(expenses_url, "#viz2", "Top Number of Tourist Expenses Ranking");
+  // plotBump(ratio_url, "#viz3", "Tourism Density Ranking");
+  // let data = await d3.json(tourist_url);
+  
+  // let chart = new d3plus.BumpChart()
+  //   .data(data)
+  //   .select("#viz1")
+  //   .groupBy("City")
+  //   .label(function(d) {
+  //     if (d.id == "circle"){
+  //       return String(d.Rank)
+  //     }
+  //     else {
+  //       return d.City;
+  //     }
+  //   })
+  //   .x("Year")
+  //   .y("Rank")
+  //   .shape(function(d) {
+  //     if (d.id === "line") {
+  //       return "Line";
+  //     }
+  //     else {
+  //       return "Circle";
+  //     }
+  //   });
+
+  // chart
+  //   .xConfig({
+  //     title: "Years",
+  //     tickSize: 15
+  //   })
+  //   .yConfig({
+  //     title: "Ranking",
+  //     tickSize: 35,
+  //     ticks: []
+  //   })
+  //   .y2Config({
+  //     tickSize: 35,
+  //     ticks: []
+  //   })
+  //   .shapeConfig({
+  //     Line: {
+  //       strokeWidth: 5
+  //     },
+  //     Circle: {
+  //       r: 17,
+  //     },
+  //     labelConfig: {
+  //       fontMin: 11
+  //     }
+  //   })
+  //   .title("Top Number of Tourists Ranking")
+  //   .titleConfig({
+  //     fontSize: 20
+  //   })
+  //   .legendPosition("top")
+  //   .render()
+  // ;
 
 
     // let expenses_data = await d3.json(expenses_url);
