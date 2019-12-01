1433c994fb50141203cb05bf10859666a191aeed
c-l-nguyen <c.l.nguyen@icloud.com>
2019-12-01 00:49:09 -0600

    added padding to the legend and fixed city colors for bump chart

--- Pirates-Travel-Index-App/static/js/functions.js
+++ Pirates-Travel-Index-App/static/js/functions.js
@@ -1,3 +1,48 @@
+function getColor(city) {
+  if (city == "Amsterdam") {
+    return "#000000";
+  } else if (city == "Antalya") {
+    return "#A6C599";
+  } else if (city == "Bangkok") {
+    return "#E9CD4D";
+  } else if (city == "Barcelona") {
+    return "#E79C8B";
+  } else if (city == "Dubai") {
+    return "#5E497B";
+  } else if (city == "Hong Kong") {
+    return "#449390";
+  } else if (city == "Istanbul") {
+    return "#983F87";
+  } else if (city == "Kuala Lumpur") {
+    return "#769047";
+  } else if (city == "London") {
+    return "#B0230F";
+  } else if (city == "Makkah") {
+    return "#F6B97C";
+  } else if (city == "Miami") {
+    return "#D1D295";
+  } else if (city == "New York") {
+    return "#234E22";
+  } else if (city == "Palma de Mallorca") {
+    return "#BDEFD1";
+  } else if (city == "Paris") {
+    return "#283069";
+  } else if (city == "Phuket") {
+    return "#DF9BCE";
+  } else if (city == "Seoul") {
+    return "#F4E38D";
+  } else if (city == "Shanghai") {
+    return "#FF69B4";
+  } else if (city == "Singapore") {
+    return "#B15C27";
+  } else if (city == "Taipei") {
+    return "#C5B6E4";
+  } else if (city == "Tokyo") {
+    return "#A9CCDE";
+  } else
+  return "grey";
+}
+
 async function plotBump(url, html_id, title){
   // url: Flask endpoint to get data from
   // html_id: HTML element to put chart in (tagged by id attribute)
@@ -30,7 +75,26 @@ async function plotBump(url, html_id, title){
       else {
         return "Circle";
       }
-  });
+    })
+    .shapeConfig({
+      Line: {
+        strokeWidth: 5,
+        stroke: function(d) {
+          return getColor(d.City);
+        }
+      },
+      Circle: {
+        r: 17,
+        fill: function(d) {
+          return getColor(d.City);
+        }
+      },
+      labelConfig: {
+        fontMin: 11,
+        fontColor: "white"
+      }
+    })
+  ;
 
   //////////////////////////////////
   // format the bump chart
@@ -50,22 +114,14 @@ async function plotBump(url, html_id, title){
       tickSize: 35,
       ticks: []
     })
-    .shapeConfig({
-      Line: {
-        strokeWidth: 5
-      },
-      Circle: {
-        r: 17,
-      },
-      labelConfig: {
-        fontMin: 11
-      }
-    })
     .title(title)
     .titleConfig({
       fontSize: 20
     })
     .legendPosition("top")
+    .legendConfig({
+      padding: 15
+    })
     .render()
   ;
 
