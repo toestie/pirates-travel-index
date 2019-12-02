69e88172358877e388911c92bfd11a67b35f17de
c-l-nguyen <c.l.nguyen@icloud.com>
2019-12-01 23:13:50 -0600

    better bump chart title

--- Pirates-Travel-Index-App/static/js/rankings.js
+++ Pirates-Travel-Index-App/static/js/rankings.js
@@ -5,7 +5,7 @@
   const expenses_url  = "/gdci-expenses";
 
   // bump charts to plot
-  plotBump(tourist_url, "#tourismRankings", "Top Number of Tourists Ranking");
-  plotBump(expenses_url, "#expensesRankings", "Top Number of Tourist Expenses Ranking");
+  plotBump(tourist_url, "#tourismRankings", "Top 10 Number of Tourists Ranking");
+  plotBump(expenses_url, "#expensesRankings", "Top 10 Number of Tourist Expenses Ranking");
 
 })()
